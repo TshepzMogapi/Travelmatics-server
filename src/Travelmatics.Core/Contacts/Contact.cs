8676ac792120e87d6f00732d21696764398794fb
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2019-09-16 10:29:21 +0200

    add contact migration

---
+++ src/Travelmatics.Core/Contacts/Contact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+
+namespace Travelmatics.Contacts
+{
+    [Table("Contacts")]
+    public class Contact : Entity, IHasCreationTime
+    {
+       
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public string EmailAddress { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+
+        public Contact()
+        {
+            CreationTime = Clock.Now;
+        }
+
+        public Contact(string firstName, string lastName = null)
+            : this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+   
+}
--- src/Travelmatics.EntityFrameworkCore/EntityFrameworkCore/TravelmaticsDbContext.cs
+++ src/Travelmatics.EntityFrameworkCore/EntityFrameworkCore/TravelmaticsDbContext.cs
@@ -3,13 +3,16 @@ using Abp.Zero.EntityFrameworkCore;
 using Travelmatics.Authorization.Roles;
 using Travelmatics.Authorization.Users;
 using Travelmatics.MultiTenancy;
+using Travelmatics.Contacts;
 
 namespace Travelmatics.EntityFrameworkCore
 {
     public class TravelmaticsDbContext : AbpZeroDbContext<Tenant, Role, User, TravelmaticsDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+
+        public virtual DbSet<Contact> Contacts { get; set; }
+
         public TravelmaticsDbContext(DbContextOptions<TravelmaticsDbContext> options)
             : base(options)
         {
